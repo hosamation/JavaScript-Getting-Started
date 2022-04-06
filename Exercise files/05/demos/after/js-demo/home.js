20cad1dfaa5ee8afe37d2926a157cd0d5e4bf2c4
MoHosam <hosamation15@gmail.com>
2022-04-06 17:19:22 +0200

    From Overview to Functions

---
+++ Exercise files/05/demos/after/js-demo/home.js
@@ -0,0 +1,35 @@
+// let welcome = "Welcome"
+// showMessage(welcome);
+
+// function logMessage() {
+//   console.log("Here is the message")
+// }
+
+// logMessage()
+// logMessage()
+
+// let myFunction = function (message, fName) {
+//   console.log(message)
+//   console.log(fName)
+// }
+
+// myFunction("Hello", "Mohammad");
+
+// let key = 42
+
+// function getSecretCode(value) {
+
+//   let keyGenerator = function() {
+//     let key = 12;
+//     console.log("in keyGenerator: ", key);
+//     return key;
+//   }
+//   let code = value * keyGenerator();
+//   console.log("in getSecretCode: ", key);
+//   return code;
+// }
+
+// let secretCode = getSecretCode(2);
+// showMessage(secretCode);
+
+changePercentoff(30);
