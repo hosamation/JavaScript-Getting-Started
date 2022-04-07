555238dd7edf39bdc0aabd2843fb15dfee78e71a
MoHosam <hosamation15@gmail.com>
2022-04-07 02:12:16 +0200

    Arrays

--- Exercise files/05/demos/after/js-demo/home.js
+++ Exercise files/05/demos/after/js-demo/home.js
@@ -63,16 +63,67 @@
 // const header = document.getElementById("message");
 // header.style.fontWeight = "800";
 
-const button = document.getElementById("see-review");
+// const button = document.getElementById("see-review");
 
-button.addEventListener("click", function(){
-  const review = document.getElementById("review");
-  if(review.classList.contains("d-none")) {
-    review.classList.remove("d-none");
-    button.textContent = "Close REVIEW";
-  } else {
-    review.classList.add("d-none");
-    button.textContent = "SEE REVIEW";
-  }
-  // console.log("click");
+// button.addEventListener("click", function(){
+//   const review = document.getElementById("review");
+//   if(review.classList.contains("d-none")) {
+//     review.classList.remove("d-none");
+//     button.textContent = "Close REVIEW";
+//   } else {
+//     review.classList.add("d-none");
+//     button.textContent = "SEE REVIEW";
+//   }
+//   // console.log("click");
+// });
+
+// ## Start Array ##
+
+const values = ["a", "b", "c"];
+values[0] = "aaa";
+console.log(values);
+console.log(typeof values);
+console.log(values[0]);
+console.log(values[1]);
+console.log(values[2]);
+console.log(values[7]);
+
+const last = values.pop();
+console.log(last);  //c
+
+const first = values.shift();
+console.log(first);  //a
+
+// values.unshift("hello");
+console.log(values);
+
+const newValues = values.slice(1, 2);
+
+console.log(newValues); 
+
+values.splice (1, 1);
+
+console.log(values); 
+
+console.log(values.indexOf("c"));
+console.log(values.indexOf("d"));
+
+const set = values.filter(function(item) {
+  return item > "b";
 });
+
+console.log(set);
+
+const found = values.find(function(item) {
+  return item.length > 1;
+});
+
+console.log(found);
+
+values.forEach(function(item) {
+  console.log(item);
+});
+
+const containers = document.getElementsByClassName("container");
+
+console.log(containers); 
